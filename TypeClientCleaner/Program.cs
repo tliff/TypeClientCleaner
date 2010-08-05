c6a93ad72341ce68697228203244bc6391a7b842
unknown <admin-maier@.verlag.vn.idowa.de>
2010-08-05 17:40:57 +0200

    Initial working version

--- TypeClientCleaner/Program.cs
+++ TypeClientCleaner/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
+using Microsoft.Win32;
+using System.Text.RegularExpressions;
+using System.Data.Common;
+using System.Data.SQLite;
 
 namespace TypeClientCleaner
 {
@@ -9,6 +12,39 @@ namespace TypeClientCleaner
     {
         static void Main(string[] args)
         {
+            Microsoft.Win32.RegistryKey key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion\Type 1 Installer\Type 1 Fonts", RegistryKeyPermissionCheck.ReadWriteSubTree);
+            string profiledir = System.Environment.GetEnvironmentVariable("USERPROFILE");
+            string dbpath = profiledir + @"\Lokale Einstellungen\Anwendungsdaten\Extensis\UTC\cache\UniversalType.db";
+            foreach (var keyname in key.GetValueNames())
+            {
+                var keyval = (string[])key.GetValue(keyname);
+                Boolean matches = false;
+                foreach (string str in keyval)
+                {
+                    if (new Regex("UTC").Split(str).Length > 1)
+                        matches = true;
+                }
+                if (matches)
+                {
+                    Console.WriteLine(keyname);
+                    //TODO: check permissions
+                    key.DeleteValue(keyname);
+                }
+            }
+
+            SQLiteConnection con = new SQLiteConnection("Data Source="+dbpath);
+            con.Open();
+            SQLiteCommand command = new SQLiteCommand(con);
+            command.CommandText = "DELETE FROM FontActivationFile";
+            command.ExecuteNonQuery();
+            command.CommandText = "DELETE FROM FontActivation";
+            command.ExecuteNonQuery();
+            command.CommandText = "UPDATE FontSet SET permActiveFontCount = 0";
+            command.ExecuteNonQuery();
+
+            Console.WriteLine(profiledir);
         }
+
     }
 }
+
